bcc35e81507cc4b3b82658ccc2194b4020095fa4
SimescuCristian <33029473+SimescuCristian@users.noreply.github.com>
2017-11-08 20:23:07 +0200

    Update Ex2.m

--- Ex2.m
+++ Ex2.m
@@ -2,13 +2,13 @@ function tema_de_casa_exercitiu1()
 
 fs = 2000;
 t = 0:1/fs:100;
-x = sawtooth(0.4*pi*t, 0.5); %semnal triunghiular
+x = triunghiular(0.4*pi*t, 0.5); %semnal triunghiular
                               %perioada 5s 
 plot(t,x);
 
 
-xlabel('Time (sec)')
-ylabel('Amplitude')
+xlabel('Timp (sec)')
+ylabel('Amplitudine')
 title('Triangular')
 
 end
